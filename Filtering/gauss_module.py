4bebaf024a5557c0b2c7fbf070824f6914523477
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2020-10-23 20:18:04 +0200

    Update gauss_module.py

--- Filtering/gauss_module.py
+++ Filtering/gauss_module.py
@@ -11,13 +11,18 @@ Gaussian function taking as argument the standard deviation sigma
 The filter should be defined for all integer values x in the range [-3sigma,3sigma]
 The function should return the Gaussian values Gx computed at the indexes x
 """
-def gauss(sigma):
-    
+
+def gauss(sigma): #sigma = 4.0
     #...
-    
-    return Gx, x
-
-
+    #Generate a vector x of values on which the Gaussian filter is defined: integer values on the interval [-3*sigma, 3*sigma]
+    low = -3*sigma
+    high = (3*sigma)+1
+    range_x = [int(i) for i in range(low,high)]
+    Gx = []
+    for x in range_x:
+        G = (math.exp((-x ** 2) / (2 * (sigma ** 2))))* (1 / math.sqrt(2 * math.pi * sigma))
+        Gx.append(G)
+    return Gx, range_x
 
 """
 Implement a 2D Gaussian filter, leveraging the previous gauss.
@@ -27,18 +32,27 @@ Input: image, sigma (standard deviation)
 Output: smoothed image
 """
 def gaussianfilter(img, sigma):
-    
     #...
-
+    #col = img.size[0]
+    #row = img.size[1]
+    #tmp_img = np.zeros((row,col)) #define an empty matrix (img) with the same dimension of the input img
+    kernel = ((13,13))
+    #fill the kernel with the values on which the G filter is defined (range [-3*sigma,3*sigma])
+    low = -3*sigma
+    high = (3*sigma)+1
+    kernel = [int(i,j) for i,j in range(low,high)]
+    #extact the first col of the kernel
+    Gx = kernel[:, 0]
+    #extract the first row of the kernel
+    Gy = kernel[0, :]    #computing the first convolution
+    tmp_img = scipy.signal.conv2d(img.flatten(),Gx,mode='full', boundary='fill', fillvalue=0)
+    #using img.flatten() because the first argument of the conv2d() must be an array, as the second one
+    #now tmp_img (the output of the first convolution) is an array
+    #computing the second convolution (on the output of the first one)
+    smooth_img = scipy.signal.conv2d(tmp_img,Gy,mode='full', boundary='fill', fillvalue=0)
+    #SOURCE: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.convolve2d.html
     return smooth_img
 
-
-
-"""
-Gaussian derivative function taking as argument the standard deviation sigma
-The filter should be defined for all integer values x in the range [-3sigma,3sigma]
-The function should return the Gaussian derivative values Dx computed at the indexes x
-"""
 def gaussdx(sigma):
 
     #...
