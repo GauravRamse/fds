af7ac42fbf446fe426193a820a9adfc53ec0ab0e
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2020-10-26 22:10:16 +0100

    Add files via upload

--- Filtering/filter.py
+++ Filtering/filter.py
@@ -1,11 +1,13 @@
 ## import packages
 import numpy as np
 from PIL import Image
-from scipy.signal import convolve2d as conv2
+from scipy.signal import convolve2d as conv2d
 import matplotlib.pyplot as plt
-
 import gauss_module
 
+
+# please provide path of image
+path_of_image_to_smoothen = r'C:\Users\Gaurav Ramse\Desktop\Data science sapienza\FDS\Assignment 1\Assignment1\Filtering\graf.png'
 
 
 def rgb2gray(rgb):
@@ -30,9 +32,12 @@ plt.show()
 
 ## function gaussianfilter (Question 1.b)
 
-img = rgb2gray(np.array(Image.open('graf.png')))
+
+img = rgb2gray(np.array(Image.open(path_of_image_to_smoothen)))
+
 smooth_img = gauss_module.gaussianfilter(img, sigma)
 
+#
 plt.figure(2)
 ax1 = plt.subplot(1,2,1)
 ax2 = plt.subplot(1,2,2)
@@ -57,40 +62,42 @@ plt.legend( ('gauss', 'gaussdx'))
 plt.show()
 
 
+# ***** we are working on this probably, This will be done by tomorrow
 
-## function gaussdx (Question 1.d)
-
-img_imp = np.zeros([27,27])
-img_imp[13, 13] = 1.0
-plt.figure(6), plt.imshow(img_imp, cmap='gray')
-
-sigma = 7.0
-[Gx, x] = gauss_module.gauss(sigma)
-[Dx, x] = gauss_module.gaussdx(sigma)
-
-Gx = Gx.reshape(1, Gx.size)
-Dx = Dx.reshape(1, Dx.size)
-
-plt.figure(7)
-plt.subplot(2,3,1)
-plt.imshow(conv2(conv2(img_imp, Gx, 'same'), Gx.T, 'same') , cmap='gray')
-plt.subplot(2,3,2)
-plt.imshow(conv2(conv2(img_imp, Gx, 'same'), Dx.T, 'same') , cmap='gray')
-plt.subplot(2,3,3)
-plt.imshow(conv2(conv2(img_imp, Dx.T, 'same'), Gx, 'same') , cmap='gray')
-plt.subplot(2,3,4)
-plt.imshow(conv2(conv2(img_imp, Dx, 'same'), Dx.T, 'same') , cmap='gray')
-plt.subplot(2,3,5)
-plt.imshow(conv2(conv2(img_imp, Dx, 'same'), Gx.T, 'same') , cmap='gray')
-plt.subplot(2,3,6)
-plt.imshow(conv2(conv2(img_imp, Gx.T, 'same'), Dx, 'same') , cmap='gray')
-plt.show()
-
-
-
+# ## function gaussdx (Question 1.d)
+#
+# img_imp = np.zeros([27,27])
+# img_imp[13, 13] = 1.0
+# plt.figure(6), plt.imshow(img_imp, cmap='gray')
+#
+# sigma = 7.0
+# [Gx, x] = gauss_module.gauss(sigma)
+# [Dx, x] = gauss_module.gaussdx(sigma)
+#
+# Gx = Gx.reshape(1, Gx.size)
+# Dx = Dx.reshape(1, Dx.size)
+#
+# plt.figure(7)
+# plt.subplot(2,3,1)
+# plt.imshow(conv2(conv2(img_imp, Gx, 'same'), Gx.T, 'same') , cmap='gray')
+# plt.subplot(2,3,2)
+# plt.imshow(conv2(conv2(img_imp, Gx, 'same'), Dx.T, 'same') , cmap='gray')
+# plt.subplot(2,3,3)
+# plt.imshow(conv2(conv2(img_imp, Dx.T, 'same'), Gx, 'same') , cmap='gray')
+# plt.subplot(2,3,4)
+# plt.imshow(conv2(conv2(img_imp, Dx, 'same'), Dx.T, 'same') , cmap='gray')
+# plt.subplot(2,3,5)
+# plt.imshow(conv2(conv2(img_imp, Dx, 'same'), Gx.T, 'same') , cmap='gray')
+# plt.subplot(2,3,6)
+# plt.imshow(conv2(conv2(img_imp, Gx.T, 'same'), Dx, 'same') , cmap='gray')
+# plt.show()
+#
+#
+#
 ## function gaussderiv (Question 1.e)
 
-img_c = np.array(Image.open('graf.png')).astype('double')
+
+img_c = np.array(Image.open(path_of_image_to_smoothen)).astype('double')
 img = rgb2gray(img_c)
 [imgDx, imgDy] = gauss_module.gaussderiv(img, 7.0)
 
@@ -106,5 +113,3 @@ plt.sca(ax3)
 imgmag = np.sqrt(imgDx**2 + imgDy**2)
 plt.imshow(imgmag, cmap='gray')
 plt.show()
-
-
--- Filtering/gauss_module.py
+++ Filtering/gauss_module.py
@@ -2,8 +2,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from scipy.signal import convolve2d as conv2
-
+from scipy.signal import convolve2d as conv2d
 
 
 """
@@ -11,19 +10,23 @@ Gaussian function taking as argument the standard deviation sigma
 The filter should be defined for all integer values x in the range [-3sigma,3sigma]
 The function should return the Gaussian values Gx computed at the indexes x
 """
-
-def gauss(sigma): #sigma = 4.0
-    #...
-    #Generate a vector x of values on which the Gaussian filter is defined: integer values on the interval [-3*sigma, 3*sigma]
-    low = -3*sigma
-    high = (3*sigma)+1
-    range_x = [int(i) for i in range(low,high)]
+def gauss(sigma):
+    # Generate a vector x of values on which the Gaussian filter is defined: integer values on the interval [-3*sigma, 3*sigma]
+    low = int(-3*sigma)
+    high = int((3*sigma))
+    range_x = [i for i in range(low, high+1)]
     Gx = []
     for x in range_x:
-        G = (math.exp((-x ** 2) / (2 * (sigma ** 2))))* (1 / math.sqrt(2 * math.pi * sigma))
+        G = (np.exp((-x ** 2) / (2 * (sigma ** 2))))* (1 / (math.sqrt(2 * math.pi)* sigma))
         Gx.append(G)
+
     return Gx, range_x
 
+Gx, _ = gauss(2)
+
+print(Gx, len(Gx))
+
+#
 """
 Implement a 2D Gaussian filter, leveraging the previous gauss.
 Implement the filter from scratch or leverage the convolve2D method (scipy.signal)
@@ -32,38 +35,66 @@ Input: image, sigma (standard deviation)
 Output: smoothed image
 """
 def gaussianfilter(img, sigma):
-    #...
-    #col = img.size[0]
-    #row = img.size[1]
-    #tmp_img = np.zeros((row,col)) #define an empty matrix (img) with the same dimension of the input img
-    kernel = ((13,13))
-    #fill the kernel with the values on which the G filter is defined (range [-3*sigma,3*sigma])
-    low = -3*sigma
-    high = (3*sigma)+1
-    kernel = [int(i,j) for i,j in range(low,high)]
-    #extact the first col of the kernel
-    Gx = kernel[:, 0]
-    #extract the first row of the kernel
-    Gy = kernel[0, :]    #computing the first convolution
-    tmp_img = scipy.signal.conv2d(img.flatten(),Gx,mode='full', boundary='fill', fillvalue=0)
-    #using img.flatten() because the first argument of the conv2d() must be an array, as the second one
-    #now tmp_img (the output of the first convolution) is an array
-    #computing the second convolution (on the output of the first one)
-    smooth_img = scipy.signal.conv2d(tmp_img,Gy,mode='full', boundary='fill', fillvalue=0)
-    #SOURCE: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.convolve2d.html
+
+    # fill the kernel with the values on which the G filter is defined (range [-3*sigma,3*sigma])
+    # extact the first row of the kernel
+    Gx = gauss(sigma)[0]
+
+    Gx = np.array(Gx)
+
+
+    Gx = Gx.reshape(1, Gx.size)   # This was done because input to the conv2d need 2s
+
+
+    # # computing the first convolution
+    tmp_img = conv2d(img, Gx, mode='full', boundary='fill', fillvalue=0,)
+    # # using img.flatten() because the first argument of the conv2d() must be an array, as the second one
+    # # now tmp_img (the output of the first convolution) is an array
+    # # computing the second convolution (on the output of the first one)
+    
+    Gy = np.transpose(Gx)
+    # print(Gy)
+    smooth_img = conv2d(tmp_img, Gy, mode='full', boundary='fill', fillvalue=0)
+    # # SOURCE: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.convolve2d.html
+
+    # return  smooth_img
+
     return smooth_img
 
+
+
+"""
+Gaussian derivative function taking as argument the standard deviation sigma
+The filter should be defined for all integer values x in the range [-3sigma,3sigma]
+The function should return the Gaussian derivative values Dx computed at the indexes x
+"""
 def gaussdx(sigma):
 
-    #...
-    
-    return Dx, x
+    low = int(-3*sigma)
+    high = int((3*sigma))
+    range_x = [i for i in range(low, high+1)]
+    Dx = []
+    for x in range_x:
+        G = -x*(np.exp((-x ** 2) / (2 * (sigma ** 2))))* (1 / (math.sqrt(2 * math.pi)* (sigma**3)))   # Just taken derivative and written formula
+        Dx.append(G)
 
-
-
+    return Dx, range_x
+#
+#
+#
 def gaussderiv(img, sigma):
 
-    #...
-    
-    return imgDx, imgDy
+    smooth_img = gaussianfilter(img, sigma)
 
+    Dx =  np.array( [[-1,0,1],   # Kernal which we will use to take derivative in x direction
+             [-1,0,1],
+             [-1,0,1]])
+    Dx = Dx/3
+
+    Dy = np.transpose(Dx) # Kernal which we will use to take derivative in y direction
+
+    imgDx = conv2d(smooth_img, Dx, mode='full', boundary='fill', fillvalue=0)  # convolving with smoothen image
+
+    imgDy = conv2d(smooth_img, Dy, mode='full', boundary='fill', fillvalue=0)  # convolving with smoothen image
+
+    return imgDx, imgDy
