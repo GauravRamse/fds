0c6fe483e607c182c760f7986bb1db7edcd5b71a
martinabetti-97 <56385024+martinabetti-97@users.noreply.github.com>
2020-11-03 21:32:40 +0100

    Update rpc_module.py

--- Identification/rpc_module.py
+++ Identification/rpc_module.py
@@ -6,79 +6,64 @@ import dist_module
 import match_module
 
 
-
 # compute and plot the recall/precision curve
 #
 # D - square matrix, D(i, j) = distance between model image i, and query image j
 #
 # note: assume that query and model images are in the same order, i.e. correct answer for i-th query image is the i-th model image
 
-def plot_rpc(D, plot_color):
-    
+def plot_rpc(D, plot_color, dist_type):
     recall = []
     precision = []
+    prec_rcll = {}  # this dictionary is to save tresholds
     num_queries = D.shape[1]
-    
+
     num_images = D.shape[0]
-    assert(num_images == num_queries), 'Distance matrix should be a square matrix'
-    
-    labels = np.diag([1]*num_images)
-      
+    assert (num_images == num_queries), 'Distance matrix should be a square matrix'
+
+    labels = np.diag([1] * num_images)
+
     d = D.reshape(D.size)
     l = labels.reshape(labels.size)
-     
-    sortidx = d.argsort()
+
+    if dist_type == 'intersect':
+        sortidx = d.argsort()[::-1][:d.shape[
+            0]]  # if distance is intersect we want values to reversed sorted list (high values give high precision)
+    else:
+        sortidx = d.argsort()  # if distance is l2 or chi2 we sort it from small to high (small values give high precision)
+
     d = d[sortidx]
     l = l[sortidx]
-    
-    tp = 0
-    #... (your code here)
-    
-    for idt in range(len(d)):
-        tp += l[idt]
-        #    prec = 0
-        recll = 0
-        threshold = 0
-        for i in range(len(d)):
-            tp = 0
-            fp = 0
-            fn = 0
-            tn = 0
-            threshold = d[i]
-            for idt in range(len(d)):
-                if d[idt] >= threshold and l[idt] == 1:
-                    tp += 1
-                elif d[idt] >= threshold and l[idt] == 0:
-                    fp += 1
-                elif d[idt] < threshold and l[idt] == 1:
-                    fn += 1
-                else:
-                    tn += 1
-            prec = (tp / (tp + fp))
-    
-    plt.plot([1-precision[i] for i in range(len(precision))], recall, plot_color+'-')
 
+    tp = 0 # Set the number of true positives as 0
+    fp = 0 # Set the number of false negative as 0
+    fn = np.sum(l) # Set the number of false negative to the matches and we will deduct the tp later
+
+    for idt in range(len(d)): # Turn in the d[]
+        tp += l[idt] # Add the correct matches for the distance lower than threshold
+        fp += 1 - l[idt] # Add the incorrect matches for the distance lower than threshold
+        fn -= l[idt] # Since we have set this as all correct matches before we should deduct tp now and get the false negatives
+
+        # Compute precision and recall values and append them to "recall" and "precision" vectors
+        precision.append(tp / (tp + fp)) # Calculate and append the precision
+        recall.append(tp / (tp + fn)) # Calculate and append the recall
+        prec_rcll[tp / (tp + fp) * tp / (tp + fn)] = d[idt]  # new value in dicitonary that saves the product between recall and precision as key and treshold as value
+    print('treshold', round(prec_rcll[max(prec_rcll)], 4),
+          ' is the best one to maximise precision and recall with value', round(max(prec_rcll), 4)) # Print out the best precision and recall product with respective threshold
+    plt.plot([1 - precision[i] for i in range(len(precision))], recall, plot_color + '-')
 
 
 def compare_dist_rpc(model_images, query_images, dist_types, hist_type, num_bins, plot_colors):
-    
     assert len(plot_colors) == len(dist_types), 'number of distance types should match the requested plot colors'
 
-    for idx in range( len(dist_types) ):
-
+    for idx in range(len(dist_types)):
+        print(dist_types[idx])
         [best_match, D] = match_module.find_best_match(model_images, query_images, dist_types[idx], hist_type, num_bins)
 
-        plot_rpc(D, plot_colors[idx])
-    
+        plot_rpc(D, plot_colors[idx], dist_types[idx])  # I added one parameter (dist_type)
 
     plt.axis([0, 1, 0, 1]);
     plt.xlabel('1 - precision');
     plt.ylabel('recall');
-    
-    # legend(dist_types, 'Location', 'Best')
-    
-    plt.legend( dist_types, loc='best')
 
-
-
-
+    plt.legend(dist_types, loc='best')
